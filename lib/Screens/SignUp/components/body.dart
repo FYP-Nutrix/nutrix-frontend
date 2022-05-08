4a9da808271df8a6487e2c271b6b87f86285d811
daniellim051000 <daniellim051000@gmail.com>
2022-05-08 14:40:27 +0800

    Sign Up Page

---
+++ lib/Screens/SignUp/components/body.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:nutrix/Screens/Login/login_screen.dart';
+import 'package:nutrix/Screens/SignUp/components/background.dart';
+import 'package:nutrix/components/already_have_an_account_check.dart';
+import 'package:nutrix/components/rounded_button.dart';
+import 'package:nutrix/components/rounded_input_field.dart';
+import 'package:nutrix/components/rounded_password_field.dart';
+
+class Body extends StatelessWidget {
+  const Body({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Sign Up',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: size.height * 0.03),
+          SvgPicture.asset(
+            "assets/images/sign_up.svg",
+            height: size.height * 0.35,
+          ),
+          SizedBox(height: size.height * 0.03),
+          RoundedInputField(
+            hintText: "Your Email",
+            onChanged: (value) {},
+          ),
+          RoundedPasswordField(
+            hintText: "Your Password",
+            onChanged: (value) {},
+          ),
+          RoundedButton(
+            text: "Sign Up",
+            press: () {},
+          ),
+          SizedBox(height: size.height * 0.03),
+          AlreadyHaveAnAccountCheck(
+            login: false,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return LoginScreen();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
