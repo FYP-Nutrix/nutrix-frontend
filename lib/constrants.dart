aab23b2ab55194ee7fcc23802ad72ef64a5cb411
daniellim051000 <daniellim051000@gmail.com>
2022-05-18 14:03:51 +0800

    add theme color

--- lib/constrants.dart
+++ lib/constrants.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFFDF643B);
 const kPrimaryLightColor = Color(0xFFE9B6A5);
+const kTextColor = Color(0xFF202E2E);
+const kTextLightColor = Color(0xFF7286A5);
